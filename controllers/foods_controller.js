b3df73a5e766cac677f0d50d76098cd9425c5a23
Mike Schutte <tmikeschutte@gmail.com>
2017-03-23 11:10:09 -0600

    Refactor server and app setup

--- controllers/foods_controller.js
+++ controllers/foods_controller.js
@@ -1,90 +1,73 @@
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../knexfile')[environment];
-const database = require('knex')(configuration);
+const Food = require('../lib/models/food');
 
-class FoodsController {
-  index(request, response) {
-    database.raw('SELECT * FROM foods')
-    .then(foods => {
-      response.json(foods.rows);
-    })
-    .catch(error => console.error(`DB problem: ${error}`));
-  }
-
-  create(request, response) {
-    const food = request.body.food;
-    const dbFoods = database.raw('SELECT * FROM foods');
-    if (!food) { return response.sendStatus(400) }
-
-    database.raw(
-      `INSERT INTO foods (name, calories, created_at, updated_at)
-      VALUES (?,?,?,?)`,
-      [ food.name, food.calories, new Date(), new Date() ]
-    )
-    .then((foods) => {
-      dbFoods.then(foods => {
-        const newFood = foods.rows[foods.rows.length - 1];
-        response.redirect(`/foods/${newFood.id}`);
-      })
-      .catch(error => console.error(`DB problem: ${error}`));
-    })
-    .catch(error => console.error(`DB problem: ${error}`));
-  }
-
-  show(request, response) {
-    database.raw('SELECT * FROM foods WHERE id=?', [request.params.id])
-    .then(foods => {
-      const food = foods.rows[0];
-      if (!food) { return response.sendStatus(404); }
-      response.json(foods.rows[0]);
-    })
-    .catch(error => console.error(`DB problem: ${error}`));
-  }
-
-  patch(request, response) {
-    const id = request.params.id;
-    const requestFood = request.body.food;
-    if (!requestFood) { return response.sendStatus(400) }
-
-    database.raw('SELECT * FROM foods WHERE id = ?', [id])
-    .then(foods => {
-      const food = foods.rows[0];
-      if (!foods.rows[0]) { return response.sendStatus(404); }
-
-      database.raw(
-        `UPDATE foods
-        SET name = ?, calories = ?, updated_at = ?
-        WHERE id = ?`,
-        [
-          requestFood.name || food.name,
-          requestFood.calories || food.calories,
-          new Date(),
-          id
-        ]
-      )
-      .then(() => {
-        response.redirect(`/foods/${id}`);
-      })
-      .catch(error => console.error(error));
-    })
-    .catch(error => console.error(`DB problem: ${error}`));
-  }
-
-  delete(request, response) {
-    const id = request.params.id;
-
-    database.raw('SELECT * FROM foods WHERE id = ?', [id])
-    .then(foods => {
-      const food = foods.rows[0];
-      if (!food) { return response.sendStatus(404); }
-      database.raw('DELETE FROM foods where id = ?', [id])
-      .then(() => {
-        response.redirect(202, '/foods');
-      })
-      .catch(error => console.error(error));
-    })
-    .catch(error => console.error(`DB problem: ${error}`));
-    }
+index = (request, response) => {
+  Food.all()
+  .then(foods =>  response.json(foods.rows))
+  .catch(error => console.error(`DB problem: ${error}`));
 }
 
-module.exports = new FoodsController();
+create = (request, response) => {
+  const food = request.body.food;
+  if (!food) { return response.sendStatus(400) }
+
+  Food.create(food)
+  .then((foods) => {
+    Food.all().then(foods => {
+      const newFood = foods.rows[foods.rows.length - 1];
+      response.redirect(`/foods/${newFood.id}`);
+    })
+    .catch(error => console.error(`DB problem: ${error}`));
+  })
+  .catch(error => console.error(`DB problem: ${error}`));
+}
+
+show = (request, response) => {
+  Food.find(request.params.id)
+  .then(foods => {
+    const food = foods.rows[0];
+    if (!food) { return response.sendStatus(404); }
+    response.json(foods.rows[0]);
+  })
+  .catch(error => console.error(`DB problem: ${error}`));
+}
+
+patch = (request, response) => {
+  const id = request.params.id;
+  const data = request.body.food;
+  if (!data) { return response.sendStatus(400) }
+
+  Food.find(id).then(foods => {
+    const food = foods.rows[0];
+    if (!foods.rows[0]) { return response.sendStatus(404); }
+
+    Food.update(data, food)
+    .then(() => {
+      response.redirect(`/foods/${id}`);
+    })
+    .catch(error => console.error(error));
+  })
+  .catch(error => console.error(`DB problem: ${error}`));
+}
+
+destroy = (request, response) => {
+  const id = request.params.id;
+
+  Food.find(id).then(foods => {
+    const food = foods.rows[0];
+    if (!food) { return response.sendStatus(404); }
+    Food.destroy(id)
+    .then(() => {
+      response.redirect(202, '/foods');
+    })
+    .catch(error => console.error(error));
+  })
+  .catch(error => console.error(`DB problem: ${error}`));
+}
+
+module.exports = {
+  index: index,
+  show: show,
+  create: create,
+  patch: patch,
+  destroy: destroy,
+};
