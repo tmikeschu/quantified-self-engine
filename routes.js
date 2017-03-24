693b4161f87382c9805fcc04f3ad4083d8c0dc03
Mike Schutte <tmikeschutte@gmail.com>
2017-03-23 19:56:46 -0600

    Refactor server design

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
-const app = require('./server');
+const app = require('./config');
 const FoodsController = require('./controllers/foods_controller');
 const HomeController = require('./controllers/home_controller');
+const RouteLister = require('./lib/helpers/list-routes');
 
 // Home Controller
 app.get('/', HomeController.index);
@@ -11,5 +12,7 @@ app.post('/api/v1/foods', FoodsController.create);
 app.get('/api/v1/foods/:id', FoodsController.show);
 app.patch('/api/v1/foods/:id', FoodsController.patch);
 app.delete('/api/v1/foods/:id', FoodsController.destroy);
+
+app.locals.routes = RouteLister.listRoutes(app);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,41 +1,9 @@
-const app = require('express')();
-const cors = require('cors');
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+const app = require('./routes');
 
-const bodyParser = require('body-parser');
-const path = require('path');
-const RouteLister = require('./lib/helpers/list-routes');
-const FoodsController = require('./controllers/foods_controller');
-const HomeController = require('./controllers/home_controller');
-
-app.options('*', cors(corsOptions))
-
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'pug');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors(corsOptions))
-
-app.locals.title = 'Quantified Self';
-
-// Home Controller
-app.get('/', HomeController.index);
-
-// Foods Controller
-app.get('/api/v1/foods', FoodsController.index);
-app.post('/api/v1/foods', FoodsController.create);
-app.get('/api/v1/foods/:id', FoodsController.show);
-app.patch('/api/v1/foods/:id', FoodsController.patch);
-app.delete('/api/v1/foods/:id', FoodsController.destroy);
-if (!module.parent) {
+if (!module.parent || !module.parent.id.includes('test')) {
   app.listen(app.get('port'), () =>{
     console.log(`${app.locals.title} is running on ${app.get('port')}`);
   });
 }
 
-app.locals.routes = RouteLister.listRoutes(app);
 module.exports = app;
